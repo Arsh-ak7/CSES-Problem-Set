80a924de6a47748d5607c5dbfcf8c6e32782efc8
Arsh-ak7 <arshkr07@gmail.com>
2021-03-20 10:42:55 +0530

    Create Missing_number.cpp

---
+++ Introductory Problems/Missing_number.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main (void)
+{
+    long long int n , t;
+    long long int sum=0;
+    scanf("%lld",&n);
+ 
+    for(long long int i=0 ; i<n-1 ; i++)
+    {
+        scanf("%lld" , &t);
+        sum+=t;
+    }
+    printf("%lld",(-sum+(n*(n+1)/2)));
+}
