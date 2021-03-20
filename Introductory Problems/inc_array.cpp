8d6951750f5a18cdf299260ff25306611994ae7e
Arsh-ak7 <arshkr07@gmail.com>
2021-03-20 12:28:01 +0530

    Added few problems

---
+++ Introductory Problems/inc_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    long arr[t];
+    for(int i =0;i<t;i++)
+    cin>>arr[i];
+    long moves = 0;
+    for(int i =0;i<t;i++){
+        if(arr[i]>arr[i+1]){
+            moves += arr[i]-arr[i+1];
+            arr[i+1]=arr[i];
+        } else
+        continue;
+    }
+    cout<<moves;
+
+}
---
+++ Introductory Problems/repetitions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    string n;
+    cin>>n;
+    long max = 0;
+    long currentSum = 0;
+    for(int i =0;i<n.length()-1;i++){
+        if(n[i]==n[i+1]){
+            currentSum+=1;
+            if(currentSum>max)
+            max= currentSum;
+        }
+        else
+        currentSum=0;
+    }
+    cout << max +1;
+}
