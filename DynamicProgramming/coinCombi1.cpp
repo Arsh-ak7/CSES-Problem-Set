cde2d49fc70036ab5e2d25d7a364559964df3ad6
Arsh-ak7 <arshkr07@gmail.com>
2021-07-17 18:12:29 +0530

    Create coinCombi1.cpp

---
+++ DynamicProgramming/coinCombi1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int MOD = 1e9 + 7;
+    int n, target;
+    cin >> n >> target;
+    vector<int> c(n);
+    for (int &v : c)
+        cin >> v;
+
+    vector<int> dp(target + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= target; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i - c[j] >= 0)
+            {
+                (dp[i] += dp[i - c[j]]) %= MOD;
+            }
+        }
+    }
+    cout << dp[target];
+}
