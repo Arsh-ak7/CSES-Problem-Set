44d0b8718efec67bcb7427912ce54a98112f8b59
Arsh-ak7 <arshkr07@gmail.com>
2021-11-30 11:39:06 +0530

    Update staticRangeSumQueriesSegmentTree.cpp

--- RangeQueries/staticRangeSumQueriesSegmentTree.cpp
+++ RangeQueries/staticRangeSumQueriesSegmentTree.cpp
@@ -1,6 +1,5 @@
 #pragma GCC optimize("O2")
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -54,5 +53,56 @@ int main()
         cout << ans << '\n';
     }
 
-    // return 0;
+    return 0;
+}
+
+vector<long long> tree;
+
+long long query(int node, int node_low, int node_high, int l, int r)
+{
+    if (l <= node_low && r >= node_high)
+    {
+        return tree[node];
+    }
+
+    if (node_high < l || r < node_low)
+        return 0;
+
+    int last_in_left = (node_low + node_high) / 2;
+
+    return query(2 * node, node_low, last_in_left, l, r) + query(2 * node + 1, last_in_left + 1, node_high, l, r);
+}
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    while (__builtin_popcount(n) != 1) // used to check if n is power of 2.
+    {
+        arr.push_back(0);
+        n++;
+    }
+
+    tree.resize(2 * n);
+
+    // Filling the leaf nodes
+    for (int i = 0; i < n; i++)
+        tree[n + i] = arr[i];
+    // Filling the non-leaf nodes in a bottom up manner
+    for (int i = n - 1; i >= 1; i--)
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+
+    while (q--)
+    {
+        int low, high;
+        cin >> low >> high;
+        low--, high--;
+        cout << query(1, 0, n - 1, low, high) << "\n";
+    }
 }
