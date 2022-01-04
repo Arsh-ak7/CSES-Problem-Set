7645e58c2c628a26d41db3ee767a6939f9a41718
Arsh-ak7 <arshkr07@gmail.com>
2022-01-04 17:03:35 +0530

    ADDED MEMO

--- DynamicProgramming/diceCombi.cpp
+++ DynamicProgramming/diceCombi.cpp
@@ -19,3 +19,56 @@ int main()
     }
     cout << dp[n];
 }
+
+// MEMOIZATION : RUNTIME ERROR AS SUM MAY BE VERY LARGE
+
+#pragma GCC optimize("O2")
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+long long MOD = 1e9 + 7;
+
+int diceCombiRecur(int arr[], int n, int dp[])
+{
+    if (dp[n] != -1)
+        return dp[n] % MOD;
+    if (n == 0)
+        return 1;
+    int count = 0;
+    for (int j = 0; j < 6; j++)
+        if (n - arr[j] >= 0)
+            count += diceCombiRecur(arr, n - arr[j], dp) % MOD;
+    return dp[n] = count % MOD;
+}
+
+signed main()
+{
+    fast_cin();
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int dp[n + 1];
+    memset(dp, -1, sizeof(dp));
+    dp[0] = 1;
+    int count = 0;
+    diceCombiRecur(arr, n, dp);
+    cout << dp[n];
+}
